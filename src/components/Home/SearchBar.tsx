489af3cb6b03d9a94549966474cfc3349c682f1b
Kraiovyi Denys <tonopik@gmail.com>
2025-09-17 15:07:58 +0300

    feat(search): finish search bar component

--- src/components/Home/SearchBar.tsx
+++ src/components/Home/SearchBar.tsx
@@ -10,16 +10,37 @@ import {
 } from '@/components/ui/drawer'
 import { ROUTES } from '@/routes'
 import { Search, X } from 'lucide-react'
+import Image from 'next/image'
+import Link from 'next/link'
 import { useEffect, useState } from 'react'
 import { Label } from '../ui/label'
-import Link from 'next/link'
-import Image from 'next/image'
+
+interface Result {
+  id: number
+  title: string
+  collection: string
+  type: string
+  price: number
+  images: [{ url: string }]
+}
 
 const SearchBar = () => {
   const [isOpen, setIsOpen] = useState(false)
   const [query, setQuery] = useState('')
-  const [results, setResults] = useState([])
+  const [notFound, setNotFound] = useState(false)
+  const [results, setResults] = useState<Result[]>([])
   const [isLoading, setIsLoading] = useState(false)
+
+  const collections = [
+    ...new Set(
+      results.map(products => {
+        return {
+          collection: products.collection,
+          type: products.type,
+        }
+      }),
+    ),
+  ]
 
   useEffect(() => {
     const handler = setTimeout(() => {
@@ -28,8 +49,9 @@ const SearchBar = () => {
       } else {
         setResults([])
       }
-    }, 1100)
+    }, 1000)
     return () => clearTimeout(handler)
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [query])
 
   const fetchProducts = async (search: string) => {
@@ -44,25 +66,34 @@ const SearchBar = () => {
       console.error('Search error:', error)
     } finally {
       setIsLoading(false)
+      if (results.length === 0) setNotFound(true)
     }
   }
 
   return (
-    <Drawer direction="top" open={isOpen} onOpenChange={setIsOpen}>
+    <Drawer
+      direction="top"
+      open={isOpen}
+      onOpenChange={() => {
+        setNotFound(false)
+        setQuery('')
+        setIsOpen(!isOpen)
+      }}
+    >
       <DrawerTrigger className="w-full">
         <div className="flex h-10 items-center justify-between bg-[#d9d9d9] px-3 text-sm opacity-60">
           <Search className="w-4" />
           <div>Search</div>
         </div>
       </DrawerTrigger>
-      <DrawerContent className="flex min-h-[35vh] justify-center gap-7 bg-black px-5 text-white">
-        <DrawerHeader>
+      <DrawerContent className="flex min-h-[35vh] overflow-auto bg-black text-white">
+        <DrawerHeader className="mt-5">
           <DrawerTitle className="text-white">Search</DrawerTitle>
           <DrawerDescription>
             Write something in the input field.
           </DrawerDescription>
         </DrawerHeader>
-        <div className="flex items-center justify-center gap-2">
+        <div className="my-10 flex items-center justify-center gap-2 px-5">
           <Label htmlFor="search">
             <Search className="w-4" />
           </Label>
@@ -75,41 +106,75 @@ const SearchBar = () => {
             value={query}
             onChange={e => setQuery(e.target.value)}
           />
-          <DrawerClose asChild>
+          <DrawerClose asChild onClick={() => setQuery('')}>
             <X className="w-6" />
           </DrawerClose>
         </div>
-        {isLoading && <p>loading........</p>}
-        {query && results.length === 0 && (
-          <div className="font-bold">No results found for {`"${query}"`}</div>
-        )}
 
-        {/* /////////// */}
-        {!isLoading &&
-          results.map(product => (
-            <Link
-              key={product.id}
-              href={`/product/${product.id}`}
-              className="flex items-center gap-3 px-3 py-2 text-sm hover:bg-gray-100"
-            >
-              {product.images[0] && (
-                <Image
-                  src={product.images[0].url}
-                  alt={product.title}
-                  width={40}
-                  height={40}
-                  className="rounded object-cover"
-                />
-              )}
-              <div>
-                <div className="font-medium">{product.title}</div>
-                <div className="text-xs text-gray-500">
-                  {product.collection || 'No collection'}
-                </div>
-                <div className="text-xs font-semibold">${product.price}</div>
+        {query.length > 1 && (
+          <>
+            {isLoading && (
+              <p className="flex animate-pulse items-center gap-2 px-5 pb-10">
+                <Search className="size-3 animate-ping" />
+                Searching . . .
+              </p>
+            )}
+            {!isLoading && notFound && results.length === 0 && (
+              <div className="px-5 pb-10">
+                No results found for{' '}
+                <span className="font-bold">{`"${query}"`}</span>
               </div>
-            </Link>
-          ))}
+            )}
+            {!isLoading && collections.length > 0 && (
+              <div className="space-y-3 border-b bg-white px-5 py-6 text-black">
+                <h3 className="tracking-widest opacity-80">Collections</h3>
+                <div className="grid grid-cols-2 gap-3 space-y-1">
+                  {collections.map((col, i) => (
+                    <Link
+                      href={'/'}
+                      key={col.collection + i}
+                      className="rounded-sm border px-3 py-1 text-sm"
+                    >
+                      <span className="opacity-70">{col.collection}</span> /{' '}
+                      {col.type.toUpperCase()}
+                    </Link>
+                  ))}
+                </div>
+              </div>
+            )}
+            {!isLoading && results.length > 0 && (
+              <div className="space-y-3 border-b bg-white px-5 py-6 text-black">
+                <h3 className="tracking-widest opacity-80">Products</h3>
+                {results.map(product => (
+                  <Link
+                    key={product.id}
+                    href={`/product/${product.id}`}
+                    className="flex items-center gap-3 px-3 py-2 text-sm hover:bg-gray-100"
+                  >
+                    {product.images[0] && (
+                      <Image
+                        src={product.images[0].url}
+                        alt={product.title}
+                        width={40}
+                        height={40}
+                        className="rounded object-cover"
+                      />
+                    )}
+                    <div>
+                      <div className="font-medium">{product.title}</div>
+                      <div className="text-xs text-gray-500">
+                        {product.collection || 'No collection'}
+                      </div>
+                      <div className="text-xs font-semibold">
+                        ${product.price}
+                      </div>
+                    </div>
+                  </Link>
+                ))}
+              </div>
+            )}
+          </>
+        )}
       </DrawerContent>
     </Drawer>
   )
